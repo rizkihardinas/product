a5c809abe57efb2efe2ff91e5bb0ad084bae462f
Rizki Hardinas Permana <36495798+rizkihardinas@users.noreply.github.com>
2025-08-13 08:34:08 +0700

    Update publish paths and Livewire component names
    Corrected the migrations publish path to use 'Database' instead of 'database', added config file publishing, and updated Livewire component class names for product and barcode lists to match their actual class names.

--- src/Providers/ProductServiceProvider.php
+++ src/Providers/ProductServiceProvider.php
@@ -26,20 +26,23 @@ class ProductServiceProvider extends ServiceProvider
             __DIR__.'/../../resources/views' => resource_path('views/vendor/product'),
         ], 'product-views');
         $this->publishes([
-            __DIR__ . '/../../database/migrations' => database_path('migrations'),
+            __DIR__ . '/../../Database/migrations' => database_path('migrations'),
         ], 'product-migrations');
+        $this->publishes([
+            __DIR__.'/../../config/rizkihardinas-product.php' => config_path('rizkihardinas-product.php'),
+        ], 'config');
     }
     protected function registerLivewireComponents()
     {
         // Product
-        Livewire::component('product.list', \Rizkihardinas\Product\Http\Livewire\Product\ListProducts::class);
+        Livewire::component('product.list', \Rizkihardinas\Product\Http\Livewire\Product\ListProduct::class);
         Livewire::component('product.create', \Rizkihardinas\Product\Http\Livewire\Product\CreateProduct::class);
         Livewire::component('product.edit', \Rizkihardinas\Product\Http\Livewire\Product\EditProduct::class);
         Livewire::component('product.detail', \Rizkihardinas\Product\Http\Livewire\Product\DetailProduct::class);
         Livewire::component('product.assign-barcode', \Rizkihardinas\Product\Http\Livewire\Product\AssignBarcode::class);
 
         // Barcode
-        Livewire::component('barcode.list', \Rizkihardinas\Product\Http\Livewire\Barcode\ListBarcodes::class);
+        Livewire::component('barcode.list', \Rizkihardinas\Product\Http\Livewire\Barcode\ListBarcode::class);
         Livewire::component('barcode.create', \Rizkihardinas\Product\Http\Livewire\Barcode\CreateBarcode::class);
         Livewire::component('barcode.edit', \Rizkihardinas\Product\Http\Livewire\Barcode\EditBarcode::class);
         Livewire::component('barcode.detail', \Rizkihardinas\Product\Http\Livewire\Barcode\DetailBarcode::class);
